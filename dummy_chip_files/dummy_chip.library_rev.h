be80e22b90e2fffe9d762d33d5e3e4878f0be14c
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2022-10-05 23:51:54 +0200

    Wrong name, must be dummy.card, not dummy.chip.
    Will upload the changed files soon.

--- dummy_chip_files/dummy_chip.library_rev.h
+++
@@ -1,6 +0,0 @@
-#define VERSION		1
-#define REVISION	1
-#define DATE		"29.9.2022"
-#define VERS		"dummy_chip.library 1.1"
-#define VSTRING		"dummy_chip.library 1.1 (29.9.2022)\r\n"
-#define VERSTAG		"\0$VER: dummy_chip.library 1.1 (29.9.2022)"
