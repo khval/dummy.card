aa5774c0e78a676922ae32354e0170f5334ab6d9
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2022-10-08 00:26:51 +0200

    Due to strange crashes, trying to use 68K stubs, random experiment.

---
+++ dummy_card_files/68k_stubs.c
@@ -0,0 +1,285 @@
+
+#define __USE_INLINE__
+
+#include <exec/exec.h>
+#include <proto/exec.h>
+#include <dos/dos.h>
+#include <exec/types.h>
+#include <exec/emulation.h>
+#include <proto/exec.h>
+#include <proto/dos.h>
+
+#define STATIC
+
+STATIC VOID stub_AllocCardMem_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_FreeCardMem_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetSwitch_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetColorArray_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetDAC_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetGC_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetPanning_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_CalculateBytesPerRow_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_CalculateMemory_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_GetCompatibleFormats_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetDisplay_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_ResolvePixelClock_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_GetPixelClock_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetClock_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetMemoryMode_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetWriteMask_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetClearMask_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetReadPlane_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_WaitVerticalSync_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetInterrupt_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_WaitBlitter_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_ScrollPlanar_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_ScrollPlanarDefault_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_UpdatePlanar_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_UpdatePlanarDefault_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_BlitPlanar2Chunky_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_BlitPlanar2ChunkyDefault_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_FillRect_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_FillRectDefault_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_InvertRect_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_InvertRectDefault_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_BlitRect_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_BlitRectDefault_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_BlitTemplate_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_BlitTemplateDefault_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_BlitPattern_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_BlitPatternDefault_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_DrawLine_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_DrawLineDefault_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_BlitRectNoMaskComplete_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_BlitRectNoMaskCompleteDefault_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_BlitPlanar2Direct_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_BlitPlanar2DirectDefault_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_EnableSoftSprite_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_EnableSoftSpriteDefault_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_AllocCardMemAbs_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetSplitPosition_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_ReInitMemory_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_Reserved2Default_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_Reserved3_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_Reserved3Default_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_WriteYUVRect_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_WriteYUVRectDefault_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_GetVSyncState_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_GetVBeamPos_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetDPMSLevel_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_ResetChip_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_GetFeatureAttrs_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_AllocBitMap_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_FreeBitMap_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_GetBitMapAttr_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetSprite_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetSpritePosition_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetSpriteImage_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetSpriteColor_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_CreateFeature_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_SetFeatureAttrs_ppc(ULONG *regarray)
+{
+}
+
+STATIC VOID stub_DeleteFeature_ppc(ULONG *regarray)
+{
+}
+
